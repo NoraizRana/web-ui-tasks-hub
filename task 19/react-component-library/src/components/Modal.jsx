7a6283615bcd8ef0f3725c2509487dd02c21d473
Noraiz Rana <noraizrana389@gmail.com>
2025-08-16 13:02:51 +0500

    Tasks Modified

--- task 19/react-component-library/src/components/Modal.jsx
+++ task 19/react-component-library/src/components/Modal.jsx
@@ -6,7 +6,7 @@ export default function DarkModeToggle({ isDark, toggle }) {
       aria-label="Toggle Dark Mode"
       onClick={toggle}
       className="ml-auto flex items-center px-2 py-2 rounded-md 
-        bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-600 
+        bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 
         transition-colors shadow text-gray-700 dark:text-gray-100"
     >
       <span className="mr-2">{isDark ? "Dark" : "Light"}</span>
